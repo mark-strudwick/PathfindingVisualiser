c0be3521142e54d737ea28f41501e74e05fd7b52
mark-strudwick <58702420+mark-strudwick@users.noreply.github.com>
2020-04-26 14:38:22 +0100

    Update astar.js
    Correctly stopping the search if no solution exists.

--- astar.js
+++ astar.js
@@ -9,6 +9,7 @@ function aStar(board) {
         visitedNodesInOrder.push(closestNode);
 
         if (closestNode.isFinish == true) return visitedNodesInOrder, grid.drawGrid();
+        if (closestNode.distance === Infinity) return visitedNodesInOrder, grid.drawGrid();
 
         grid.grid[closestNode.column][closestNode.row].getNeighbours();
         unvisitedNeighbours = closestNode.neighbours;
